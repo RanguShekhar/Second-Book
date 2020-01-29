dfb2fecfa132160ced43f014031867785e244b51
nafi1635 <nazeemoddin.cs1635@gmail.com>
2020-01-29 09:10:18 +0530

    Updating registration

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent, HomeComponent, LoginComponent],
+  declarations: [NavbarComponent, HomeComponent, LoginComponent, RegisterComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([])
